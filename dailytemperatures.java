a926acbda8f66edb2750d8b1ef17707ffa3978c3
Kevin Dai <kevin.dai.2451@gmail.com>
2020-09-21 12:46:36 -0400

    added java solution

---
+++ dailytemperatures.java
@@ -0,0 +1,18 @@
+//Java Solution
+
+class Solution {
+    public int[] dailyTemperatures(int[] T) {
+        Stack<Integer> stack = new Stack<>();
+        int[] result = new int[T.length];
+        
+        for (int i = 0; i < T.length; i++) {
+            while (!stack.isEmpty() && T[i] > T[stack.peek()]) {
+                int index = stack.pop();
+                result[index] = i - index;
+            }
+            stack.push(i);
+        }
+        
+        return result;
+    }
+}
